9a600cf4862e1cf0eae8ee9098e05f5d979a206d
DiYeix3m <66112605+DiYeix3m@users.noreply.github.com>
2025-02-03 12:22:09 -0600

    Update ___init___.py
    Signed-off-by: DiYeix3m <66112605+DiYeix3m@users.noreply.github.com>

--- components/wvc/___init___.py
+++ components/wvc/___init___.py
@@ -27,7 +27,7 @@ CONFIG_SCHEMA = cv.Schema(
 ).extend(uart.UART_DEVICE_SCHEMA)
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
+    var = cg.new_Pvariable(config[CONF_WVC_ID])
     await cg.register_component(var, config)
     await uart.register_uart_device(var, config)
 
