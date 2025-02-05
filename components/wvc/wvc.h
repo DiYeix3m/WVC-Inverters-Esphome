9549ece5fb296b5784f18cec54b92873b9504c3f
DiYeix3m <66112605+DiYeix3m@users.noreply.github.com>
2025-02-05 13:08:35 -0600

    add model sensor
    add support to R2 WVC295, WVC300, WVC350, WCV600, WVC850 and WVC1200
    
    Signed-off-by: DiYeix3m <66112605+DiYeix3m@users.noreply.github.com>

--- components/wvc/wvc.h
+++ components/wvc/wvc.h
@@ -6,7 +6,7 @@ namespace esphome {
 namespace wvc {
 
 class WVCComponent : public uart::UARTDevice, public Component {
- public:
+public:
   void setup() override;
   void loop() override;
 
@@ -14,7 +14,6 @@ class WVCComponent : public uart::UARTDevice, public Component {
   void set_inverter_type(const std::string &type);
   void set_throttle(uint32_t throttle) { this->throttle_ = throttle; }
 
-  int16_t temp_r2_lookup(int16_t value);
   void set_vac_sensor(sensor::Sensor *sensor);
   void set_aac_sensor(sensor::Sensor *sensor);
   void set_vdc_sensor(sensor::Sensor *sensor);
@@ -24,16 +23,16 @@ class WVCComponent : public uart::UARTDevice, public Component {
   void set_dcw_sensor(sensor::Sensor *sensor);
   void set_temperature_sensor(sensor::Sensor *sensor);
   void set_serial_number_text_sensor(text_sensor::TextSensor *serial_number_text_sensor) {
-    serial_number_text_sensor_ = serial_number_text_sensor;
-    }
+   serial_number_text_sensor_ = serial_number_text_sensor;
+  }
   void set_hardware_revision_text_sensor(text_sensor::TextSensor *hardware_revision_text_sensor) {
-    hardware_revision_text_sensor_ = hardware_revision_text_sensor;
-    }
+   hardware_revision_text_sensor_ = hardware_revision_text_sensor;
+  }
 
  protected:
   void parse_response(const std::string &response);
   void publish_state_once_(text_sensor::TextSensor *text_sensor, const std::string &state);
-
+  float r2_values(std::string type, uint16_t value);
   
  private:
   sensor::Sensor *vac_sensor_{nullptr};
@@ -46,6 +45,7 @@ class WVCComponent : public uart::UARTDevice, public Component {
   sensor::Sensor *temperature_sensor_{nullptr};
   text_sensor::TextSensor *hardware_revision_text_sensor_{nullptr};
   text_sensor::TextSensor *serial_number_text_sensor_{nullptr};
+  text_sensor::TextSensor *model_text_sensor_{nullptr};
   uart::UARTComponent *uart_;
 
   bool waiting_for_response = false;
@@ -54,10 +54,10 @@ class WVCComponent : public uart::UARTDevice, public Component {
 
   std::string inverter_sn_;
   std::string inverter_type_;
+  std::string inverter_model_;
   uint32_t throttle_{0};  
   void send_command(uint8_t *command, size_t length, uint8_t expected_start_byte_, size_t expected_length_, const std::string &device_id);
 };
-
 
 }  // namespace wvc
 }  // namespace esphome
